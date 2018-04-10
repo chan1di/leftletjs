6a081c852e495b77a61920c56cfaf76370107bb7
chan1di <you@example.com>
2018-04-10 20:49:44 +0530

    modeified yml 15

---
+++ mup.js
@@ -0,0 +1,30 @@
+module.exports = {
+  servers: {
+    one: {
+      "host": "139.59.33.198",
+      "username": "root",
+      "pem": "/Users/user/.ssh/id_rsa"
+    }
+  },
+
+  meteor: {
+    name: 'app-name',
+    path: '/Applications/',
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true
+    },
+    env: {
+      "NODE_ENV": "production",
+      "PORT": "3005",
+      "ROOT_URL": "http://",
+      "MAIL_URL": "smtp://",
+      "MONGO_URL": "mongodb://"
+    },
+    dockerImage: 'abernix/meteord:node-8.4.0-base',
+    deployCheckWaitTime: 120,
+    enableUploadProgressBar: true
+  }
+};
